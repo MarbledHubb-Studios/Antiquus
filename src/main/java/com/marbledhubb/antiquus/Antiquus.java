200231ec50142a8edd9650696da34ae384e87653
MarbledNull <cyandoezstuff@gmail.com>
2026-07-21 17:58:35 -0500

    finished mod set up

--- src/main/java/com/marbledhubb/antiquus/Antiquus.java
+++ src/main/java/com/marbledhubb/antiquus/Antiquus.java
@@ -1,5 +1,8 @@
 package com.marbledhubb.antiquus;
 
+import com.marbledhubb.antiquus.init.ModBlocks;
+import com.marbledhubb.antiquus.init.ModItems;
+import com.mojang.logging.LogUtils;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.Mod;
@@ -8,16 +11,22 @@ import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.server.ServerStartingEvent;
+import org.slf4j.Logger;
 
 @Mod(Antiquus.MODID)
 public class Antiquus {
     public static final String MODID = "antiquus";
+    public static final Logger LOGGER = LogUtils.getLogger();
 
     public Antiquus(IEventBus modEventBus, ModContainer modContainer) {
         modEventBus.addListener(this::commonSetup);
         NeoForge.EVENT_BUS.register(this);
 
+
         modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+
+        ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
     }
 
     private void commonSetup(FMLCommonSetupEvent event) {
