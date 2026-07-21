570e83056c5d3b2f676110716f9719e37571a4a8
MarbledNull <cyandoezstuff@gmail.com>
2026-07-21 17:58:22 -0500

    prototaxite block progress (not working)

--- src/main/java/com/marbledhubb/antiquus/init/ModBlocks.java
+++ src/main/java/com/marbledhubb/antiquus/init/ModBlocks.java
@@ -1,4 +1,25 @@
 package com.marbledhubb.antiquus.init;
 
+import com.marbledhubb.antiquus.Antiquus;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredBlock;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+import java.util.function.Function;
+
 public class ModBlocks {
+    public static DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(Antiquus.MODID);
+
+    //public static final DeferredBlock<Block> PROTOTAXITE_STEM = BLOCKS.registerBlock("prototaxite_stem", properties -> new Block(BlockBehaviour.Properties.of().sound(SoundType.FUNGUS).strength(2f)));
+
+    public static <T extends Block> DeferredBlock<T> registerBlock(String name, Function<BlockBehaviour.Properties,T> function) {
+        return BLOCKS.registerBlock(name, function);
+    }
+
+    public static void register(IEventBus eventBus) {
+        BLOCKS.register(eventBus);
+    }
 }
--- src/main/java/com/marbledhubb/antiquus/init/ModItems.java
+++ src/main/java/com/marbledhubb/antiquus/init/ModItems.java
@@ -1,4 +1,18 @@
 package com.marbledhubb.antiquus.init;
 
+import com.marbledhubb.antiquus.Antiquus;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
 public class ModItems {
+    public static DeferredRegister.Items ITEMS = DeferredRegister.createItems(Antiquus.MODID);
+
+    //public static final DeferredItem<Item> PROTOTAXITE_STEM = ITEMS.registerItem("prototaxite_stem.json", properties -> new BlockItem(ModBlocks.PROTOTAXITE_STEM.get(), properties));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
 }
